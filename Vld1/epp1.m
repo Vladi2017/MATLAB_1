392787b7286cd88d9decdec47be2d72d57a9e400
Vladi Manolescu <mvmanol@yahoo.com>
2021-08-26 14:03:42 +0300

    Solved X axis.., and default resolution r.. Test OK..

--- Vld1/epp1.m
+++ Vld1/epp1.m
@@ -1,18 +1,17 @@
 ## Copyright (C) 2021 Vladimir Manolescu
 
 ## -*- texinfo -*-
-## @deftypefn  {} {} epp (@var{outcomes}, @var{r})
-## @deftypefnx {} {} epp (@var{outcomes}, @var{X})
+## @deftypefn  {} {} epp (@var{outcomes}[, @var{r}])
+## @deftypefnx {} {} @var{pdfVec} = epp (@var{outcomes}[, @var{r}])
 ## Plot probability density function (PDF) shape for empirical univariate
 ## @var{outcomes} at resolution @var{r}. epp = empirical_pdf_plot.
 ##
-## @var{r} is natural scalar >= 1.
-## When 2nd argument @var{X} is a vector, resolution is length(@var{X}).
+## @var{r} is natural scalar >= 1, with default value 64.
 ## @subheading Usage example:
 ## @example
 ## @group
 ## exp1 = exprnd(1, 1000);
-## epp(exp1, 20);
+## epp(exp1, 128);
 ## @end group
 ## @end example
 ## @end deftypefn
@@ -20,7 +19,7 @@
 ## Author: MVM <mvmanol@yahoo.com>
 ## Description: Empirical PDF shape.
 
-function pdfV = epp1(outcomes, r)
+function pdfV = epp1(outcomes, r = 64)
   min1 = min(min(outcomes));
   max1 = max(max(outcomes));
   N = numel(outcomes);
@@ -36,7 +35,7 @@ function pdfV = epp1(outcomes, r)
   pdfabs(r) = N - sum(pdfabs);
   pdfabs(r+1) = pdfabs(r);
   pdf = pdfabs / N;
-  X = 0 : r;
+  X = min1 : delta : max1;
   stairs (X, pdf);
   ylim([0 max(pdf)]);
   if nargout == 1

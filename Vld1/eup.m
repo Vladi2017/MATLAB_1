3629473036708c0e7f6d6f7c819e72d959ce1659
VladiManolescu <mvmanol@yahoo.com>
2022-06-04 03:57:11 +0300

    Introduced support for multiple plots in the same figure thus to compare different distributions..
    We may have:
    octave:138> exp1 = exprnd(1, 1000);
    octave:139> exp3 = exprnd(3, 1000);
    octave:140> whos;
    Variables visible from the current scope:
    variables in scope: top scope
       Attr Name        Size                     Bytes  Class
       ==== ====        ====                     =====  =====
            ans         1x1                          8  double
            exp1     1000x1000                 8000000  double
            exp3     1000x1000                 8000000  double
    octave:141> eup(exp1, 128, 1);
    octave:142> eup(exp3, 128, 1);
    octave:143> subplot(2,1,1); xlim([0 20]);
    octave:144> subplot(2,1,2); xlim([0 20]);
    
    But.., although it is obvious that sum(pdfV) = 1, the area behind distribution graphic is not 1 as it should be.. (in order to compare two distributions)..
    In this respect it is a delta(s) issue.., which should be an "absolute" number, common for both distributions..
    Otherwise only the distribution's shape(s) are relevant.., but not density itself (y-axis)..
    I hope to be more clear in the next commit(s)..

--- Vld1/eup.m
+++ Vld1/eup.m
@@ -1,19 +1,25 @@
 ## Copyright (C) 2021 Vladimir Manolescu
 
 ## -*- texinfo -*-
-## @deftypefn  {} {} epp (@var{outcomes}[, @var{r}])
-## @deftypefnx {} {} @var{pdfVec} = epp (@var{outcomes}[, @var{r}])
-## @deftypefnx {} {} [@var{pdfVec}, @var{cdfVec}] = epp (@var{outcomes}[, @var{r}])
+## @deftypefn  {} {} eup (@var{outcomes}[, @var{r}[, @var{fig}]])
+## @deftypefnx {} {} @var{pdfVec} = eup (@var{outcomes}[, @var{r}[, @var{fig}]])
+## @deftypefnx {} {} [@var{pdfVec}, @var{cdfVec}] = eup (@var{outcomes}[, @var{r}[, @var{fig}]])
 ## Plot probability density function (PDF) and cumulative DF (CDF) shape for
-## empirical univariate @var{outcomes} at resolution @var{r}.
+## empirical univariate @var{outcomes} at resolution @var{r}. New plots may be added in the same figure
+## with @var{fig} parameter.
 ## eup = empirical_univariate_plot.
 ##
 ## @var{r} is natural scalar >= 1, with default value 64.
-## @subheading Usage example:
+## @subheading DEMOs:
 ## @example
 ## @group
+## pkg load statistics; #unless loaded already
 ## exp1 = exprnd(1, 1000);
-## epp(exp1, 128);
+## eup(exp1, 128);
+## 
+## 
+## 
+## 
 ## @end group
 ## @end example
 ## @end deftypefn
@@ -21,7 +27,7 @@
 ## Author: MVM <mvmanol@yahoo.com>
 ## Description: Empirical probability plots.
 
-function [pdfV, cdfV] = eup(outcomes, r = 64)
+function [pdfV, cdfV] = eup(outcomes, r = 64, fig = 1000)
   min1 = min(min(outcomes));
   max1 = max(max(outcomes));
   N = numel(outcomes);
@@ -38,20 +44,35 @@ function [pdfV, cdfV] = eup(outcomes, r = 64)
   pdfabs(r+1) = pdfabs(r);
   pdf = pdfabs / N;
   X = min1 : delta : max1;
-  figure;
+  if (fig == 1000)
+    figure;
+  else
+    figure(fig);
+    hold on;
+  endif
   subplot(2,1,1);
-  stairs (X, pdf);
+  if (fig != 1000)
+    hold on;
+  endif
+  stairs (X, pdf, "displayname", inputname(1));
+  legend off;
   ylim([0 max(pdf)]); title("PDF");
   if nargout >= 1
     pdfV = pdf(1:end-1); #pdfVec, sum(pdfV) should be 1.
   endif
+  legend show;
+  hold off;
   cdf(1) = pdf(1);
   for k = 2 : r
     cdf(k) = cdf(k-1) + pdf(k); #cdf = ∫pdf
   endfor
   cdf(r+1) = cdf(r);
   subplot(2,1,2);
+  if (fig != 1000)
+    hold on;
+  endif
   stairs (X, cdf); title("CDF");
+  hold off;
   if nargout == 2
     cdfV = cdf(1:end-1); #cdfVec, cdfV(end) should be 1.
   endif
